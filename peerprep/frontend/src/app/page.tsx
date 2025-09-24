6036bd51e30f47a73a563d8c105c1b339e48c208
sannie-beep <81695359+sannie-beep@users.noreply.github.com>
2025-09-24 16:34:39 +0800

    Set color name variables in global.css

--- peerprep/frontend/src/app/page.tsx
+++ peerprep/frontend/src/app/page.tsx
@@ -46,41 +46,41 @@ export default function LoginPage() {
 
   return (
     <div 
-      className="bg-[#050325] h-screen w-screen flex items-center justify-center"
+      className="bg-dark-blue-bg h-screen w-screen flex items-center justify-center"
     >
-      <div className="bg-[#2119566E] p-16 rounded-4xl w-[650px] h-[632px] flex flex-col items-center justify-between">
+      <div className="bg-dark-box p-16 rounded-4xl w-[650px] h-[632px] flex flex-col items-center justify-between">
         <div className="flex items-center justify-center flex-col space-y-4">
           <div className="flex flex-row">
-            <span className="font-inter text-[#6E5AE2] text-8xl font-bold ">Peer</span>
-            <span className="font-inter text-[#5ae2c6] text-8xl font-bold ">Prep</span>
+            <span className="font-inter text-logo-purple text-8xl font-bold ">Peer</span>
+            <span className="font-inter text-logo-green text-8xl font-bold ">Prep</span>
           </div>
-          <p className="font-poppins text-[#FFFFFF87] text-xl font-medium">Ace that interview. Together.</p>
+          <p className="font-poppins text-text-grey text-xl font-medium">Ace that interview. Together.</p>
         </div>
         <div className="flex flex-col items-start justify-center w-4/5">
-          <p className="font-poppins text-[#958AD5] text-xl font-medium m-1 pl-1">Email/Username</p>
+          <p className="font-poppins text-text-main text-xl font-medium m-1 pl-1">Email/Username</p>
           <input 
             ref={usernameOrEmailRef}
             type="text" 
-            className="w-full h-[55px] rounded-2xl p-4 mb-3 bg-[#FFFFFF6B] focus:border-[#6E5AE2] focus:border-2 focus:outline-none text-[#3F3C4D] placeholder-[#3F3C4D] font-poppins text-xl font-medium" 
+            className="w-full h-[55px] rounded-2xl p-4 mb-3 bg-text-area focus:border-purple-outline focus:border-2 focus:outline-none text-text-field placeholder-text-field font-poppins text-xl font-medium" 
             placeholder="Enter email or username" />
-          <p className="font-poppins text-[#958AD5] text-xl font-medium m-1 pl-1">Password</p>
+          <p className="font-poppins text-text-main text-xl font-medium m-1 pl-1">Password</p>
           <input 
             ref={passwordRef}
             type="password" 
-            className="w-full h-[55px] rounded-2xl p-4 bg-[#FFFFFF6B] focus:border-[#6E5AE2] focus:border-2 focus:outline-none text-[#3F3C4D] placeholder-[#3F3C4D] font-poppins text-xl font-medium" 
+            className="w-full h-[55px] rounded-2xl p-4 bg-text-area focus:border-purple-outline focus:border-2 focus:outline-none text-text-field placeholder-text-field font-poppins text-xl font-medium" 
             placeholder="Enter password" />
         </div>
         {isError && (
-          <p className="font-poppins text-[#d36a6a] text-xl font-medium m-1 pl-1">Invalid email/username or password</p>
+          <p className="font-poppins text-text-error text-xl font-medium m-1 pl-1">Invalid email/username or password</p>
         )}
         <div className="w-full flex flex-row justify-between items-center">
           <button 
-            className="bg-[#2F0B6D] w-[215px] h-[58px] rounded-xl text-white font-poppins text-3xl font-medium hover:border-[#6E5AE2] hover:border-2 hover:bg-[#330b78]"
+            className="bg-blue-button w-[215px] h-[58px] rounded-xl text-white font-poppins text-3xl font-medium hover:border-logo-purple hover:border-2 hover:bg-blue-button-hover"
             onClick={handleLogIn}
           >Log in</button>
           <Link href="/signup">
             <button 
-              className="bg-linear-to-r from-[#7316D7] to-[#0B6D59] w-[215px] h-[58px] rounded-xl text-white font-poppins text-3xl font-medium hover:border-[#6E5AE2] hover:border-2"
+              className="bg-linear-to-r from-purple-button to-dg-button w-[215px] h-[58px] rounded-xl text-white font-poppins text-3xl font-medium hover:border-logo-purple hover:border-2"
             >
               <div className="flex flex-row justify-center items-center space-x-2">
                 <span> Sign up </span>
